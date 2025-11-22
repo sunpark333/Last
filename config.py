b29a3178f8d1899998fe6db23965506784e0c1ce
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-11-22 15:31:44 +0530

    Remove hardcoded BOT_TOKEN from config

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ YTUB_COOKIES = """
 
 API_ID = int(getenv("API_ID", "20373203"))
 API_HASH = getenv("API_HASH", "8962717c7c708e210f66ea658db58d85")
-BOT_TOKEN = getenv("BOT_TOKEN", "7121079088:AAFmztuhSDgaMdbWwyiYFDHoWtbeLCZhEXc")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7968732891").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://wikeke7922:VMoBcxdkgJwjeCaK@cluster0.sexvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002596328286")
